54a3ecbe0e1d4837ba37fe035a3f856da828b9f0
deepakkumar <deepak752820@gmail.com>
2025-08-05 13:40:10 +0530

    ✅ Secure setup and PostgreSQL config

--- rojgar_project/settings.py
+++ rojgar_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+
+from django.contrib import staticfiles
 from dotenv import load_dotenv
 import dj_database_url
 
